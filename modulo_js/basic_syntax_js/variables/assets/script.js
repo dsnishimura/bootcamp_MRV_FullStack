9dfcf117a0c35a15900b5f3190b0250a7a409ed1
dsnishimura <nishionthego@gmail.com>
2022-01-23 16:55:40 -0300

    Added new JS2 classes' little programs!

---
+++ modulo_js/basic_syntax_js/variables/assets/script.js
@@ -0,0 +1,20 @@
+//Primitive Types
+
+// //Boolean
+// var Vboo = false;
+// console.log(typeof(Vboo));
+
+// //var
+// var v1 = 'lol';
+// v1 = 'hehe';
+// console.log(v1)
+
+// //Comparative
+// var v2 = 5 < 2;
+// console.log(v2)
+
+//Comparative 2
+var v3 = 5 > 2;
+console.log(v2)
+
+
