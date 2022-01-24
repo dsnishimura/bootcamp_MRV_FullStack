7af7d16c7a5b8d5dd7d5ec508d839c5878ec1293
dsnishimura <nishionthego@gmail.com>
2022-01-24 16:17:39 -0300

    Added arrays_objects folders!

---
+++ modulo_js/basic_syntax_js/arrays_objects/script.js
@@ -0,0 +1,22 @@
+let array =  ['string',1,true, ['array1', 'lol'], ['array2']];
+console.log(array)
+
+
+//Função forEach() intera um array
+
+// array;array.forEach(function(item, index){console.log(item, item)
+// });
+
+// array.splice(3,4)
+// console.log(array)
+
+//objects
+
+var mug = {
+    name : 'mug_StarWars',
+    color : 'white',
+    size : 'M',
+    full : true
+}
+
+console.log(mug.name)
